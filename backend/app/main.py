824ab185a651efd4ce68d1f749ff7ea5aa76a2a6
eje019 <emricndayake@gmail.com>
2026-02-24 10:02:15 +0000

    **Appris dans cette session : - Lancer un serveur
    -Comprendre ce qu'est une route
    
    - Modifier une API
    
    - Ajouter une nouvelle fonctionnalité

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,4 +4,8 @@ app = FastAPI(title="Student Analytics API")
 
 @app.get("/")
 def root():
-    return {"message": "API Student Analytics"}
+    return {"message": "C'est maginifique"}
+
+@app.get("/test")
+def test():
+    return{"message": "on est sur la page test"}

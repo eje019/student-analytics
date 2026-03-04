ca2b1b31635c52343632f7ec0939c9993cfdab10
eje019 <emricndayake@gmail.com>
2026-03-04 14:38:48 +0000

    Test : creation d'une table

--- backend/app/models.py
+++ backend/app/models.py
@@ -8,3 +8,10 @@ class User(Base):
     email = Column(String, unique=True, index=True)
     name = Column(String)
     hashed_password = Column(String)
+
+
+# class User(Base):
+#     __tablename__= "test"
+
+#     id = Column(Integer, primary_key=True, index=True)
+#     name = Column(String, unique=True, index=True)

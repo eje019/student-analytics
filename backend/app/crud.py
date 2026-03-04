56772122839f39203db8f98cd218a1bf1ed0640f
eje019 <emricndayake@gmail.com>
2026-03-04 14:46:09 +0000

    Refactor create_user function for clarity and consistency

--- backend/app/crud.py
+++ backend/app/crud.py
@@ -3,6 +3,16 @@ from . import models
 
 
 def create_user(db: Session, name: str):
+    #cree un objet user avec le nom que lutilisateur vas fournir
     db_user = models.User(name=name)
 
+    #on ajoute cet objet user la
     db.add(db_user)
+
+    db.commit()
+
+    #on recupere lid genere automatiquement
+    db.refresh(db_user)
+
+    return db_user
+
